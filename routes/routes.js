989582989153bd3238b2bc403090a64382355d03
Deepak345 <senapati.dipak97@gmail.com>
2018-03-11 11:34:07 +0530

    server

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var studentLogin = require("../controllers/studentlogin");
+//var studentLogin = require("../controllers/studentlogin");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'Express' });
+    res.render('pages/index', { title: 'Express' });
 });
 
-router.get("/student", studentLogin );
+router.get("/student", function(req, res, next) {
+    res.render('pages/student', { title: 'Student Dashboard' });
+});;
 
 module.exports = router;
